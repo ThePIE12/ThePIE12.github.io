027fa3c8c0b12bc678b32bb6757d72307a0fb527
tiredcoffee <Nicky01@live.se>
2023-06-15 17:01:16 +0200

    Adjusted the text for the language buttons & adjusted all files styling

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 function loadNewContent(link) {
-    $('#main-wrapper *[langrpl]').each(function(i, e) {
-        const loadHolder = $('<div></div>');
-        const qs = `*[langrpl=${$(this).attr('langrpl')}]`;
+	$('#main-wrapper *[langrpl]').each(function(i, e) {
+		const loadHolder = $('<div></div>');
+		const qs = `*[langrpl=${$(this).attr('langrpl')}]`;
 
-        loadHolder.load(`${link} ${qs}`, function() {
-            $('#main-wrapper').find(qs).html(loadHolder.find(qs).html());
-        });
-    });
+		loadHolder.load(`${link} ${qs}`, function() {
+			$('#main-wrapper').find(qs).html(loadHolder.find(qs).html());
+		});
+	});
 }
 
 const createPartialLoadAndReplaceLanguage = () => {
 	let running = false;
 
 	const animateContentChange = (duration, offset, callback) => {
-	    setTimeout(function() {
-	        $('#clone').css('opacity', '0');
-	        $('#clone').find('*[langrpl]').css('opacity', '0');
-	    }, offset);
+		setTimeout(function() {
+			$('#clone').css('opacity', '0');
+			$('#clone').find('*[langrpl]').css('opacity', '0');
+		}, offset);
 
-	    setTimeout(function() {
-	        $('#main-wrapper').find('*[langrpl]').css('opacity', '1');
-	    }, duration/2 + offset);
+		setTimeout(function() {
+			$('#main-wrapper').find('*[langrpl]').css('opacity', '1');
+		}, duration / 2 + offset);
 
-	    setTimeout(() => {
-	        $('#main-wrapper').css('position', 'initial').css('z-index', 'initial');
-	        $('#main-wrapper').find('*[langrpl]').removeClass('in-able').css('opacity', 'initial');
-	        $('#clone').remove();
-	        callback();
-	    }, duration + offset);
+		setTimeout(() => {
+			$('#main-wrapper').css('position', 'initial').css('z-index', 'initial');
+			$('#main-wrapper').find('*[langrpl]').removeClass('in-able').css('opacity', 'initial');
+			$('#clone').remove();
+			callback();
+		}, duration + offset);
 	}
 
 	return (link, duration = 700, offset = 50) => {
@@ -50,19 +50,21 @@ const createPartialLoadAndReplaceLanguage = () => {
 		}, offset);
 
 		loadNewContent(link);
-		animateContentChange(duration, offset, function () { running = false });
+		animateContentChange(duration, offset, function() {
+			running = false
+		});
 	}
 }
 
 const partialLoadAndReplaceLanguage = createPartialLoadAndReplaceLanguage();
 
 function onDocLoaded(context = document) {
-    $('.langbtn', context).click(function(e) {
-        e.preventDefault();
-        $('.langbtn', context).parent().removeAttr('activelang');
-        $(this).parent().attr('activelang', '');
-        partialLoadAndReplaceLanguage(this.href);
-    });
+	$('.langbtn', context).click(function(e) {
+		e.preventDefault();
+		$('.langbtn', context).parent().removeAttr('activelang');
+		$(this).parent().attr('activelang', '');
+		partialLoadAndReplaceLanguage(this.href);
+	});
 }
 
-$( document ).ready(onDocLoaded);
+$(document).ready(onDocLoaded);
